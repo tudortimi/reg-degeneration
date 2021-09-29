38842ce4d5c6e74ddb1dbe7fa07bbef8ceb1ec42
Tudor Timi <tudortimi@users.noreply.github.com>
2021-09-29 17:07:39 +0200

    Add dummy main function as script entry point

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ import setuptools
 setuptools.setup(
     name="reg-degeneration",
     python_requires=">=3.6",
+    entry_points = {
+        'console_scripts': ['reg-degeneration=reg_degeneration:main'],
+    }
 )
