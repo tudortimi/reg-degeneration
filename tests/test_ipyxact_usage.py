301f0d7609837167f092d41d85a4e0da43362de4
Tudor Timi <tudortimi@users.noreply.github.com>
2021-09-30 12:23:28 +0200

    Read reduced IP-XACT description of UART 16550
    Currently, we're reading it as a regular XML (to ensure that it's
    well-formed), but we'll read it into `ipyxact`.

--- tests/test_ipyxact_usage.py
+++ tests/test_ipyxact_usage.py
@@ -1,8 +1,8 @@
+import xml.etree.ElementTree as ET
 import reg_degeneration
 
 
-def test_read_uart_16550(tmp_path):
-    uart_16550 = tmp_path / 'uart_16550.xml'
-    content = 'will be filled with IP-XACT description'
-    uart_16550.write_text(content)
-    print(uart_16550.resolve())
+def test_read_uart_16550():
+    tree = ET.parse('uart_16550.xml')
+    root = tree.getroot()
+    print(root)
