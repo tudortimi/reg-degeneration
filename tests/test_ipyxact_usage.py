d783009deeccba7df2a6b57202c65ac25354d4ff
Tudor Timi <tudortimi@users.noreply.github.com>
2021-09-30 13:41:13 +0200

    Add checks on objects created by `ipyxact`
    These serve as examples of how to query the objects.

--- tests/test_ipyxact_usage.py
+++ tests/test_ipyxact_usage.py
@@ -5,4 +5,30 @@ from ipyxact.ipyxact import Component
 def test_read_uart_16550():
     uart_16550 = Component()
     uart_16550.load('uart_16550.xml')
-    print(uart_16550.name)
+    assert uart_16550.name == 'uart_16550'
+    assert uart_16550.memoryMaps
+    assert len(uart_16550.memoryMaps.memoryMap) == 1
+
+    register_map = uart_16550.memoryMaps.memoryMap[0]
+    assert register_map.name == 'RegisterMap'
+    assert register_map.addressBlock
+    assert len(register_map.addressBlock) == 1
+
+    sfrs = register_map.addressBlock[0]
+    assert sfrs.name == 'Special Function Registers'
+    assert sfrs.register
+    assert len(sfrs.register) == 2
+
+    lcr = sfrs.register[0]
+    assert lcr.name == 'LCR'
+    assert lcr.addressOffset == int('0x100c', 0)
+    assert lcr.field
+    assert len(lcr.field) == 4
+
+    wls = lcr.field[0]
+    assert wls.name == 'WLS'
+    assert wls.bitWidth == 2
+    assert wls.access == 'read-write'
+    assert wls.resets
+    assert wls.resets.reset
+    assert wls.resets.reset.value == 0
