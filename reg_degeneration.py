c19ac59a125b4deeccc41254f8ac1391f8d2a23e
Tudor Timi <tudortimi@users.noreply.github.com>
2021-10-01 11:11:54 +0200

    Extract function that returns UVM access from IP-XACT

--- reg_degeneration.py
+++ reg_degeneration.py
@@ -1,13 +1,19 @@
 from pyuvm import uvm_reg_field
 
 
+def get_access(ipyxact_field):
+    assert ipyxact_field.access == 'read-write'
+    # TODO Implement handling of oll accesses
+    return 'RW'
+
+
 def add_uvm_reg_field(reg, ipyxact_field):
     field = uvm_reg_field(ipyxact_field.name)
     setattr(reg, ipyxact_field.name, field)
     field.configure(reg,
                     ipyxact_field.bitWidth,
                     ipyxact_field.bitOffset,
-                    'RW',  # TODO Handle access
+                    get_access(ipyxact_field),
                     False,  # TODO Handle volatility
                     ipyxact_field.resets.reset.value)
 
